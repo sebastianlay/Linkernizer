2dd6e93be57e22a1a2f399fa3368d67fb0931236
Sebastian Lay <mail@sebastian-lay.de>
2026-05-08 22:30:44 +0200

    Improve code structure

---
+++ Linkernizer/Internal/Structures.cs
@@ -0,0 +1,30 @@
+﻿namespace Linkernizer.Internal;
+
+/// <summary>
+/// Represents the state passed to <see cref="string.Create{TState}"/> for constructing the output string.
+/// </summary>
+/// <param name="Input">The complete input text.</param>
+/// <param name="Replacements">The list of replacements to be made.</param>
+/// <param name="DefaultScheme">The default scheme to prepend for links without a scheme.</param>
+internal readonly record struct State(string Input, List<Replacement> Replacements, string DefaultScheme);
+
+/// <summary>
+/// Represents a part of the original input that will be replaced.
+/// </summary>
+/// <param name="Offset">The offset to the start of the original input.</param>
+/// <param name="Length">The length of the match in the original input.</param>
+/// <param name="Type">The type of the match that determines how the match will be replaced in the output.</param>
+internal readonly record struct Replacement(int Offset, int Length, ReplacementType Type);
+
+/// <summary>
+/// Represents the type of match.
+/// </summary>
+internal enum ReplacementType
+{
+  InternalWithScheme,
+  InternalWithoutScheme,
+  ExternalWithScheme,
+  ExternalWithoutScheme,
+  EmailWithScheme,
+  EmailWithoutScheme
+}
